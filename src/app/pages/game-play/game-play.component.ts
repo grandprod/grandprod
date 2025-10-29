214c1b399ee85b4e9ac513f58be9c1250df65f22
Kyle J. Kemp <kyle@seiyria.com>
2025-10-29 12:09:40 -0500

    add play view

--- src/app/pages/game-play/game-play.component.ts
+++ src/app/pages/game-play/game-play.component.ts
@@ -1,13 +1,20 @@
 import { Component, computed } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { PlayMonstersComponent } from '@/app/play-monsters/play-monsters.component';
+import { PlayStatsComponent } from '@/app/play-stats/play-stats.component';
 import { OptionsBaseComponent } from '@components/panel-options/option-base-page.component';
 import { TeleportOutletDirective } from '@directives/teleport.outlet.directive';
 import { getOption } from '@helpers';
 
 @Component({
   selector: 'app-game-play',
-  imports: [RouterModule, TeleportOutletDirective],
+  imports: [
+    RouterModule,
+    TeleportOutletDirective,
+    PlayMonstersComponent,
+    PlayStatsComponent,
+  ],
   templateUrl: './game-play.component.html',
   styleUrl: './game-play.component.scss',
 })
